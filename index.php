e70ebd5b0160b0ebbdb73a80440f3aa04835772d
Jeffrey Brink <JeffreyBrink@GMX.com>
2018-01-11 15:51:22 -0700

    added flow list

--- index.php
+++ index.php
@@ -1,9 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
 	<head>
-		<link rel="stylesheet" href="stylesheet.css">
 		<meta charset="utf-8"/>
-		<title>Data-design</title>
+		<title>Data-Design</title>
 	</head>
 	<body>
 		<h1>Persona</h1>
@@ -17,20 +16,21 @@
 		<h2>User story:
 			<br>Ted likes to contribute to the site, as well as be informed by others on the site as well.</h2>
 		<h2>use case:
-				 Ted posted his new blog on the most sought after Fender Stratacaster.  Later in the day, he checks back
-				 and sees he has recieved twenty seven claps.</h2>
+					Ted posted his new blog on the most sought after Fender Stratacaster.
+					<br>Later in the day, he checks back
+					and sees he has recieved twenty seven claps.</h2>
 		<h2>precondition:  Ted is a user in good standing on the site.
 			<br>postcondition:  Ted feels great that people liked his blog post.
 		</h2>
-		<h2>Interaction flow:</h2>h2>
-		<div>
-			<ul>
-				<li> ted logs into his account</li>
-				<li> ted logs into his account</li>
-				<li> ted logs into his account</li>
-				<li> ted logs into his account</li>
-
-			</ul>
-		</div>
+		<h2>Interaction flow:</h2>
+		<ul>
+			<li>Ted logs into his account</li>
+			<li>Ted clicks the button to create a blog post.</li>
+			<li>Ted writes his blog post</li>
+			<li>Ted clicks button to post his new blog.</li>
+			<li>Ted logs out</li>
+			<li>Ted logs in later to see that he has recieved 274 claps</li>
+			<li>Ted is happy</li>
+		</ul>
 	</body>
 </html>
