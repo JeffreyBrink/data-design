fac9a809a270b66aea49e83af94a8c4317dda88f
Jeffrey Brink <JeffreyBrink@GMX.com>
2018-01-10 22:35:51 -0700

    increased text size

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
 	<head>
+		<link rel="stylesheet" href="stylesheet.css">
 		<meta charset="utf-8"/>
 		<title>Basic HTML Page</title>
 	</head>
 	<body>
-		<h1>Hello, World! :D</h1>
-
+		<h1>Data Design</h1>
+		<img src="ear.jpg" align="middle">
 	</body>
 </html>
