3f5c3b093a6484b7f6c0fb6ed9f626bec2482b97
Jeffrey Brink <JeffreyBrink@GMX.com>
2018-01-11 15:29:30 -0700

    adding persona information

--- index.php
+++ index.php
@@ -3,11 +3,34 @@
 	<head>
 		<link rel="stylesheet" href="stylesheet.css">
 		<meta charset="utf-8"/>
-		<title>Basic HTML Page</title>
+		<title>Data-design</title>
 	</head>
 	<body>
-		<h1>Data Design</h1>
-		<h2>Persona and usage cases pending</h2>
-		<img src="ear.jpg" align="middle">
+		<h1>Persona</h1>
+		<h2>Ted Random
+
+			Male, age 25</h2>
+		<h2> tech usage:
+			<br>uses a pc desktop to produce music for his band
+			<br>uses a pc laptop to record live at gigs/events.
+			<br>spends alot of time writing/reading medium's music content.</h2>
+		<h2>User story:
+			<br>Ted likes to contribute to the site, as well as be informed by others on the site as well.</h2>
+		<h2>use case:
+				 Ted posted his new blog on the most sought after Fender Stratacaster.  Later in the day, he checks back
+				 and sees he has recieved twenty seven claps.</h2>
+		<h2>precondition:  Ted is a user in good standing on the site.
+			<br>postcondition:  Ted feels great that people liked his blog post.
+		</h2>
+		<h2>Interaction flow:</h2>h2>
+		<div>
+			<ul>
+				<li> ted logs into his account</li>
+				<li> ted logs into his account</li>
+				<li> ted logs into his account</li>
+				<li> ted logs into his account</li>
+
+			</ul>
+		</div>
 	</body>
 </html>
